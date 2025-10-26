c4febcc0af3889d00484b8e383373b650374f2f6
User1012007 <rojasbadilloe@gmail.com>
2025-10-25 18:04:38 -0600

    mappings: add map to move n chars with [HJKL]

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "H", "5h", { desc = "move left 5 chars" })
+map("n", "L", "5l", { desc = "move right 5 chars" })
+map("n", "J", "5j", { desc = "move down 5 lines" })
+map("n", "K", "5k", { desc = "move up 5 lines" })
+
+map("v", "H", "5h", { desc = "move left 5 chars" })
+map("v", "L", "5l", { desc = "move right 5 chars" })
+map("v", "J", "5j", { desc = "move down 5 lines" })
+map("v", "K", "5k", { desc = "move up 5 lines" })
+
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
