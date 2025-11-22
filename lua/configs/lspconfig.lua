33a68845f29e8f441c786f9b5c730c38ce1b2d43
User1012007 <rojasbadilloe@gmail.com>
2025-11-21 21:42:14 -0600

    lspconfig: update to the new api

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,25 +1,34 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
---
--- local lspconfig = require "lspconfig"
---
--- -- EXAMPLE
--- local servers = { "html", "cssls" }
--- local nvlsp = require "nvchad.configs.lspconfig"
--- lspconfig.clang.setup {}
---
--- -- lsps with default config
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---   }
--- end
+local nvlsp = require("nvchad.configs.lspconfig")
+nvlsp.defaults()
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+vim.lsp.config.clangd = {
+  cmd = { 'clangd' },
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+  root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
+  capabilities = nvlsp.capabilities,
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+  callback = function(args)
+    local root = vim.fs.root(args.buf, { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' })
+    
+    if not root then
+      root = vim.fn.getcwd()
+    end
+    
+    vim.lsp.start({
+      name = 'clangd',
+      cmd = { 'clangd' },
+      root_dir = root,
+      capabilities = nvlsp.capabilities,
+    })
+  end,
+})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
