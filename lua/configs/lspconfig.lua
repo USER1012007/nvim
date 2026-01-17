bd49ba5ebdc39c702802a14dc953f0e810ea02dd
User1012007 <rojasbadilloe@gmail.com>
2026-01-17 15:54:50 -0600

    lspconfig.lua: add rust-analyzer support for rust files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,36 @@
 local nvlsp = require("nvchad.configs.lspconfig")
 nvlsp.defaults()
 
+vim.lsp.config.rust_analyzer = {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', 'rust-project.json', '.git' },
+  capabilities = nvlsp.capabilities,
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'rust',
+  callback = function(args)
+    local root = vim.fs.root(args.buf, { 'Cargo.toml', 'rust-project.json', '.git' })
+
+    vim.lsp.start({
+      name = 'rust_analyzer',
+      cmd = { 'rust-analyzer' },
+      root_dir = root,
+      settings = {
+        ['rust-analyzer'] = {
+          checkOnSave = true,
+          check= {
+            command = "clippy",
+          },
+          procMacro = { enable = true },
+        },
+      },
+      capabilities = nvlsp.capabilities or vim.lsp.protocol.make_client_capabilities(),
+    })
+  end,
+})
+
 vim.lsp.config.clangd = {
   cmd = { 'clangd' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
