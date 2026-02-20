51ad14b349c8b0f1aa1c5d624b3c6133fcd3daab
User1012007 <rojasbadilloe@gmail.com>
2026-02-20 09:23:16 -0600

    palette.lua: add extra color

---
+++ lua/user/palette.lua
@@ -0,0 +1,25 @@
+-- ~/.config/nvim/lua/user/palette.lua
+local M = {}
+
+M.bg = "#0b0a12"
+M.bg2 = "#12101f"
+M.panel = "#1a1530"
+M.sel_bg = "#2a1f4d"
+
+M.fg = "#ebe6ff"
+M.fg_dim = "#c4bddf"
+M.comment = "#8f87b2"
+
+M.purple = "#9d72ff"
+M.purple2 = "#c3a6ff"
+M.purple3 = "#6f4ec2"
+M.violet = "#4b347f"
+M.magenta = "#d58aff"
+M.rose = "#e6b0ff"
+
+M.error = "#ff7ab3"
+M.warn = "#caa2ff"
+M.info = "#9d72ff"
+M.hint = "#7a60c8"
+
+return M
